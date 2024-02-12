32c8f58e4c4420d7be7c188ee35b844ab34a6f8f
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2024-02-12 22:27:05 +0100

    Add code for List Todo Page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { cache } = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -7,7 +8,7 @@ const PORT = 8000;
 const app = express();
 
 // Dtabase connection
-const connectionUrl = "mongodb://127.0.0.1:27017/schoolDb";
+const connectionUrl = "mongodb://127.0.0.1:27017/todoDb";
 mongoose
   .connect(connectionUrl)
   .then(() => {
@@ -20,6 +21,16 @@ mongoose
 // view engine
 app.set("view engine", "ejs");
 
+//
+app.get("/", (req, res, next) => {
+  try {
+    res.render("");
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
 
 // listen server
-app.listen(PORT, () => {console.log(`Server listening on ${PORT}`)});
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
