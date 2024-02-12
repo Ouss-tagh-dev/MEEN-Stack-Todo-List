a41549eed59abe959041bd423bb1f4b4f128f9c5
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2024-02-12 22:14:21 +0100

    Connect with Database

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = 8000;
 
 // init app
 const app = express();
+
+// Dtabase connection
+const connectionUrl = "mongodb://127.0.0.1:27017/schoolDb";
+mongoose
+  .connect(connectionUrl)
+  .then(() => {
+    console.log("Database  Connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 // view engine
 app.set("view engine", "ejs");
